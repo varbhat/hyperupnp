7797caf3071af83e96377ae378925dc4078a1580
Var Bhat <mailvarbhat@gmail.com>
2022-12-02 12:00:46 +0530

    Feat: Preview changes every 10s

--- app/src/main/java/app/vbt/hyperupnp/SquareVideoView.kt
+++ app/src/main/java/app/vbt/hyperupnp/SquareVideoView.kt
@@ -1,8 +1,12 @@
 package app.vbt.hyperupnp
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
+import android.util.Log
 import android.widget.VideoView
+
 
 class SquareVideoView : VideoView {
     constructor(context: Context) : super(context)
@@ -17,5 +21,32 @@ class SquareVideoView : VideoView {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val width = measuredWidth
         setMeasuredDimension(width, width)
+    }
+
+    private val fhandler = Handler(Looper.getMainLooper())
+    private val delay = 10000L
+    private var shouldRepeat = true
+
+
+    override fun onAttachedToWindow() {
+        fhandler.postDelayed(object : Runnable {
+            override fun run() {
+                Log.d("repeater", "preview seeking")
+                var d = duration
+                if (d < 0) d = 0
+                seekTo((0..d).random())
+                if (shouldRepeat) {
+                    fhandler.postDelayed(this, delay)
+                } else {
+                    Log.d("repeater", "stopped preview seeking")
+                }
+            }
+        }, delay)
+        super.onAttachedToWindow()
+    }
+
+    override fun onDetachedFromWindow() {
+        shouldRepeat = false
+        super.onDetachedFromWindow()
     }
 }
